b80bb3fa04377148344560c961c840ac24a82837
Labib2003 <labib.salimi30@gmail.com>
2022-05-11 16:36:06 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors);
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p5bxt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
